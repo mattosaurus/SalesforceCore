f50998c1e3fef4c96674f56f4d45280920d562f3
mattosaurus <bitscry@outlook.com>
2020-05-22 09:25:03 +0100

    Add sync MessageBatch

--- SalesforceCore/Api/PushApi.cs
+++ SalesforceCore/Api/PushApi.cs
@@ -35,6 +35,30 @@ namespace SalesforceCore.Api
         /// <param name="body">JSON Parameters</param>
         /// <returns>Message</returns>
         ApiResponse<Message> CreateMessageWithHttpInfo(Message body);
+
+        /// <summary>
+        /// sendMessageBatch
+        /// </summary>
+        /// <remarks>
+        /// Sends unique messages to devices within the same API call. Each batch can include a maximum of 5000 subscriber key or device token values, depending on which value the call uses.
+        /// </remarks>
+        /// <exception cref="SalesforceCore.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="messageId">The ID of the message to send</param>
+        /// <param name="body">JSON Parameters</param>
+        /// <returns>Campaign</returns>
+        MessageBatch SendMessageBatch(string messageId, MessageBatch body);
+
+        /// <summary>
+        /// sendMessageBatch
+        /// </summary>
+        /// <remarks>
+        /// Sends unique messages to devices within the same API call. Each batch can include a maximum of 5000 subscriber key or device token values, depending on which value the call uses.
+        /// </remarks>
+        /// <exception cref="SalesforceCore.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="messageId">The ID of the message to send</param>
+        /// <param name="body">JSON Parameters</param>
+        /// <returns>Message</returns>
+        ApiResponse<MessageBatch> SendMessageBatchWithHttpInfo(string messageId, MessageBatch body);
         #endregion Synchronous Operations
 
         #region Asynchronous Operations
@@ -159,7 +183,6 @@ namespace SalesforceCore.Api
         {
             ApiResponse<Message> localVarResponse = await CreateMessageAsyncWithHttpInfo(body);
             return localVarResponse.Data;
-
         }
 
         /// <summary>
@@ -220,6 +243,82 @@ namespace SalesforceCore.Api
             return new ApiResponse<Message>(localVarStatusCode,
                 localVarResponse.Headers.ToDictionary(x => x.Name, x => x.Value.ToString()),
                 (Message)this.Configuration.ApiClient.Deserialize(localVarResponse, typeof(Message)));
+        }
+
+        /// <summary>
+        /// sendMessageBatch
+        /// </summary>
+        /// <remarks>
+        /// Sends unique messages to devices within the same API call. Each batch can include a maximum of 5000 subscriber key or device token values, depending on which value the call uses.
+        /// </remarks>
+        /// <exception cref="SalesforceCore.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="messageId">The ID of the message to send</param>
+        /// <param name="body">JSON Parameters</param>
+        /// <returns>Campaign</returns>
+        public MessageBatch SendMessageBatch(string messageId, MessageBatch body)
+        {
+            ApiResponse<MessageBatch> localVarResponse = SendMessageBatchWithHttpInfo(messageId, body);
+            return localVarResponse.Data;
+        }
+
+        /// <summary>
+        /// sendMessageBatch
+        /// </summary>
+        /// <remarks>
+        /// Sends unique messages to devices within the same API call. Each batch can include a maximum of 5000 subscriber key or device token values, depending on which value the call uses.
+        /// </remarks>
+        /// <exception cref="SalesforceCore.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="messageId">The ID of the message to send</param>
+        /// <param name="body">JSON Parameters</param>
+        /// <returns>Message</returns>
+        public ApiResponse<MessageBatch> SendMessageBatchWithHttpInfo(string messageId, MessageBatch body)
+        {
+            // verify the required parameter 'messageId' is set
+            if (messageId == null)
+                throw new ApiException(400, "Missing required parameter 'messageId' when calling PushApi->SendMessageBatch");
+
+            // verify the required parameter 'body' is set
+            if (body == null)
+                throw new ApiException(400, "Missing required parameter 'body' when calling PushApi->SendMessageBatch");
+
+            var localVarPath = "/push/v1/messageBatch/{messageId}/send";
+            var localVarPathParams = new Dictionary<String, String>();
+            var localVarQueryParams = new List<KeyValuePair<String, String>>();
+            var localVarHeaderParams = new Dictionary<String, String>(this.Configuration.DefaultHeader);
+            var localVarFormParams = new Dictionary<String, String>();
+            var localVarFileParams = new Dictionary<String, FileParameter>();
+            Object localVarPostBody = null;
+
+            // to determine the Content-Type header
+            String[] localVarHttpContentTypes = new String[] {
+            };
+            String localVarHttpContentType = this.Configuration.ApiClient.SelectHeaderContentType(localVarHttpContentTypes);
+
+            // to determine the Accept header
+            String[] localVarHttpHeaderAccepts = new String[] {
+            };
+            String localVarHttpHeaderAccept = this.Configuration.ApiClient.SelectHeaderAccept(localVarHttpHeaderAccepts);
+            if (localVarHttpHeaderAccept != null)
+                localVarHeaderParams.Add("Accept", localVarHttpHeaderAccept);
+
+            if (messageId != null) localVarPathParams.Add("messageId", this.Configuration.ApiClient.ParameterToString(messageId)); // path parameter
+
+            // make the HTTP request
+            IRestResponse localVarResponse = (IRestResponse)this.Configuration.ApiClient.CallApi(localVarPath,
+                Method.DELETE, localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarFileParams,
+                localVarPathParams, localVarHttpContentType);
+
+            int localVarStatusCode = (int)localVarResponse.StatusCode;
+
+            if (ExceptionFactory != null)
+            {
+                Exception exception = ExceptionFactory("SendMessageBatch", localVarResponse);
+                if (exception != null) throw exception;
+            }
+
+            return new ApiResponse<MessageBatch>(localVarStatusCode,
+                localVarResponse.Headers.ToDictionary(x => x.Name, x => x.Value.ToString()),
+                null);
         }
     }
 }
